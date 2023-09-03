9836527706d8129cadcbadd017b47c65cf23642c
king-of-barry <blebari1@hwemail.com>
2023-09-03 13:17:25 -0700

    made the Blob File, still need to add it into the folder.

--- Blob.java
+++ Blob.java
@@ -1,7 +1,61 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.*;
+import java.io.*;
 
 public class Blob {
+
+    private Path p;
+    private String pathName;
+    private File file;
+
+    public Blob(String name) {
+
+        this.pathName = name;
+        Path p = Paths.get(name);
+        file = new File(name);
+    }
+
+    public String fileContents() throws IOException // based off Chris' code in fileTOString since his code allows me to
+                                                    // retrieve multiple lines
+    {
+        StringBuilder record = new StringBuilder("");
+        BufferedReader br = new BufferedReader(new FileReader(pathName));
+
+        while (br.ready()) {
+            record.append((char) br.read());
+        }
+
+        br.close();
+        String s = record.toString();
+        return s;
+    }
+
+    public void makeFile() throws NoSuchAlgorithmException, IOException {
+
+        PrintWriter pw = new PrintWriter(getSha1(fileContents())); // found out online from Java Oracle and Danny and I
+        // disc
+        // makes a file under fileName
+
+        String words = fileContents();
+
+        pw.print(words);
+
+        pw.close(); // releases the info
+
+        // System.out.println(words);
+
+    }
 
     public String getSha1(String input) throws NoSuchAlgorithmException { // credit to
                                                                           // http://www.sha1-online.com/sha1-java/
@@ -14,4 +68,5 @@ public class Blob {
 
         return sb.toString();
     }
+
 }
--- Tester.java
+++ Tester.java
@@ -3,11 +3,12 @@ import java.util.*;
 import java.io.*;
 
 public class Tester {
-    public static void main(String[] args) throws NoSuchAlgorithmException {
+    public static void main(String[] args) throws NoSuchAlgorithmException, IOException {
 
-        Blob a = new Blob();
+        Blob a = new Blob("/Users/lilbarbar/Desktop/Honors Topics/Programming-Git/balls.txt");
         System.out.println();
-        System.out.println(a.getSha1("a"));
+        System.out.println(a.getSha1(""));
         System.out.println();
+        a.makeFile();
     }
 }
