4869fa1f572bedb9ca998c838eb5c69ed2a1679e
danyul <danieltonyh06@gmail.com>
2023-09-13 15:51:10 -0700

    added remove(String type) method
    remove either a blob or a tree from an the according array list

--- Tree.java
+++ Tree.java
@@ -21,4 +21,42 @@ public class Tree {
         }
     }
 
+    // if the string contains ".txt" then you must be tyring to remove via file name
+    // loop through the blobs
+    // loop through the trees
+    // note that if type is a sha1 file, you must loop through both the trees and
+    // the blob array list
+    public boolean remove(String type) {
+        if (type.contains(".txt")) {
+            String temp = "";
+            for (String s : blobs) {
+                temp = s.substring(50);
+                if (temp.equals(type))
+                    blobs.remove(s);
+                return true;
+            }
+        } else {
+            String temp = "";
+            for (String s : trees) {
+                temp = s.substring(7);
+                if (temp.equals(type)) {
+                    trees.remove(s);
+                    return true;
+                } else {
+                    temp = "";
+                }
+            }
+            for (String s : blobs) {
+                temp = s.substring(7, 50);
+                if (temp.equals(type)) {
+                    blobs.remove(s);
+                    return true;
+                } else {
+                    temp = "";
+                }
+            }
+
+        }
+        return false;
+    }
 }
