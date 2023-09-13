18543f3e54de1ccbc5d98bc52af3b61beb9d41bf
danyul <danieltonyh06@gmail.com>
2023-09-13 15:38:44 -0700

    created the add(String type) method
    adds a string in the appropriate format to an array list of trees or blobs

--- Tree.java
+++ Tree.java
@@ -8,4 +8,17 @@ public class Tree {
 
     }
 
+    // if type does not have "tree : " or "blob : " it must not be a valid string
+    // input
+    // if type contains "tree : " add it to the array list of trees and vice versa
+    public void add(String type) {
+        if (type.contains("tree : ") || type.contains("blob : ")) {
+            if (type.contains("tree : ")) {
+                trees.add(type);
+            } else if (type.contains("blob : ")) {
+                blobs.add(type);
+            }
+        }
+    }
+
 }
