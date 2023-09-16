91a809368aa1cfa1ab89f9bd40ed15e6192d35df
danyul <danieltonyh06@gmail.com>
2023-09-16 12:08:34 -0700

    added testIndexBlob () junit test

--- GitTest.java
+++ GitTest.java
@@ -6,6 +6,7 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -16,6 +17,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class GitTest {
+    // expected sha1 values after creating blobs
+    static String[] sha1 = { "5a1fd7b393a24123b1b7dda32a0272ed490a89d6", "dcb33305c60ffa6a5b38dd58172dbdf04c2c7b48",
+            "c766df3281b44ba77ed851278fd9398ffac07c58", "0b5b083670146187f7dd447b04ca08079a54a1a8",
+            "7e3e7a1554dc8dc7f3659fea93063e2763edd4bc" };
+
+    // pre blob expected contents
+    static String[] contents = { "test file contents 1", "test file contents 2", "test file contents 3",
+            "test file contents 4", "test file contents 5" };
+
     @BeforeAll
     static void setUpBeforeClass() throws Exception {
         PrintWriter pw1 = new PrintWriter("junit_example_file_data1.txt");
@@ -63,15 +73,8 @@ public class GitTest {
     }
 
     @Test
-    @DisplayName("[15] Test if adding a blob works. 5 for sha, 5 for file contents, 5 for correct location")
+    @DisplayName("[15] Test if creating a blob works. 5 for sha, 5 for file contents, 5 for correct location")
     void testCreateBlob() throws Exception {
-        // expected sha1 hash values
-        String[] sha1 = { "5a1fd7b393a24123b1b7dda32a0272ed490a89d6", "dcb33305c60ffa6a5b38dd58172dbdf04c2c7b48",
-                "c766df3281b44ba77ed851278fd9398ffac07c58", "0b5b083670146187f7dd447b04ca08079a54a1a8",
-                "7e3e7a1554dc8dc7f3659fea93063e2763edd4bc" };
-        // pre blob expected contents
-        String[] expectedContents = { "test file contents 1", "test file contents 2", "test file contents 3",
-                "test file contents 4", "test file contents 5" };
         try {
             Index index = new Index();
             // create the 5 blobs
@@ -83,14 +86,13 @@ public class GitTest {
         } catch (Exception e) {
             System.out.println("An error ocurred: " + e.getMessage());
         }
-        // check if the blob exists in the objects folder
 
-        // check the sha1 hashes
+        // check the sha1 hashes and the correct location (inside the objects folder)
         Path p1 = Paths.get("objects", sha1[0]);
-        Path p2 = Paths.get("objects", sha1[0]);
-        Path p3 = Paths.get("objects", sha1[0]);
-        Path p4 = Paths.get("objects", sha1[0]);
-        Path p5 = Paths.get("objects", sha1[0]);
+        Path p2 = Paths.get("objects", sha1[1]);
+        Path p3 = Paths.get("objects", sha1[2]);
+        Path p4 = Paths.get("objects", sha1[3]);
+        Path p5 = Paths.get("objects", sha1[4]);
         assertTrue(Files.exists(p1));
         assertTrue(Files.exists(p2));
         assertTrue(Files.exists(p3));
@@ -98,6 +100,37 @@ public class GitTest {
         assertTrue(Files.exists(p5));
 
         // check if the file contents are the same
+        String content1 = readFile(p1.toString(), StandardCharsets.UTF_8);
+        String content2 = readFile(p1.toString(), StandardCharsets.UTF_8);
+        String content3 = readFile(p1.toString(), StandardCharsets.UTF_8);
+        String content4 = readFile(p1.toString(), StandardCharsets.UTF_8);
+        String content5 = readFile(p1.toString(), StandardCharsets.UTF_8);
+        assertEquals(contents[0], content1);
+        assertEquals(contents[1], content2);
+        assertEquals(contents[2], content3);
+        assertEquals(contents[3], content4);
+        assertEquals(contents[4], content5);
+    }
+
+    @Test
+    @DisplayName("[3] Test if adding a blob updates the index file.")
+    void testIndexBlob() throws Exception {
+        try {
+            Index index = new Index();
+            // create the 5 blobs
+            index.add("junit_example_file_data1.txt");
+            index.add("junit_example_file_data2.txt");
+            index.add("junit_example_file_data3.txt");
+            index.add("junit_example_file_data4.txt");
+            index.add("junit_example_file_data5.txt");
+        } catch (Exception e) {
+            System.out.println("An error ocurred: " + e.getMessage());
+        }
+        Path indexPath = Paths.get("index.txt");
+        String indexContents = readFile(indexPath.toString(), StandardCharsets.UTF_8);
+        for (int i = 0; i < 4; i++) {
+            assertTrue(indexContents.contains(contents[i]));
+        }
 
     }
 
